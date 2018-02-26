bae53a59bcfb135f6700cebfeed3849c6ccdc836
kazu-mottan <kaitoxtennis1997mail@gmail.com>
2018-02-26 16:12:31 +0900

    Gitチュートリアルの追加

---
+++ python/fizzbuzz.py
@@ -0,0 +1,12 @@
+i = 1
+while i < 101;
+    if % 15 == 0:
+        print("Fizz buzz!")
+    elif i % 3 == 0:
+        print("Fizz!")
+    elif i % 5 == 0:
+        print("buzz!")
+    else:
+        print(i)
+    
+    i += 1
